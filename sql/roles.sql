09e33f04d11701bc2df37fd88fd612299122f4bc
yoga220802 <yogaagustiansyah01@gmail.com>
2024-12-16 12:44:13 +0700

    create roles and users

---
+++ sql/roles.sql
@@ -0,0 +1,5 @@
+-- Tabel roles (ID dikustomisasi, tipe data string)
+CREATE TABLE roles (
+    roleID VARCHAR PRIMARY KEY NOT NULL,
+    roleName VARCHAR NOT NULL
+);
---
+++ sql/transactionInfo.sql
@@ -0,0 +1,10 @@
+-- Tabel transactionInfo (ID custom format "dd-mm-yyyy-increment")
+CREATE TABLE transactionInfo (
+    trID VARCHAR PRIMARY KEY NOT NULL, -- Format custom
+    trDate TIMESTAMP NOT NULL,
+    total INTEGER NOT NULL,
+    payment paymentType NOT NULL,
+    change INTEGER,
+    buyerName VARCHAR,
+    userID UUID REFERENCES users(userID)
+);
